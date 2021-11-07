8fdd25c4c046e5eff2a484c8390e35294ce3b049
ledugani <tdugan85@gmail.com>
2021-11-07 13:23:25 -0600

    nice.

--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -4,30 +4,32 @@ import { Image } from 'react-bootstrap';
 
 function Home () {
     return (
-        <header className='home-header'> 
+        <header> 
             <Image src={logo} fluid />
-            <h1>
-                <strong>
-                    ledugani.com
-                </strong>
-            </h1>
-            <p>
-                <strong>d.</strong> graphic art & illustrations by artist, developer, and creator &nbsp;
-                <em>Tom Dugan</em>
-            </p>
-            <a href='http://tduganenterprise.com'>
-                <font face='arial' size='3' color='blue'>
-                    WEBSITE DEVELOPMENT SERVICES
-                </font>
-            </a>
+            <div className='home-header'>
+                <h1>
+                    <strong>
+                        ledugani.com
+                    </strong>
+                </h1>
+                <p>
+                    <strong>d.</strong> graphic art & illustrations by artist, developer, and creator &nbsp;
+                    <em>Tom Dugan</em>
+                </p>
+                <a href='http://tduganenterprise.com'>
+                    <font face='arial' size='3' color='blue'>
+                        WEBSITE DEVELOPMENT SERVICES
+                    </font>
+                </a>
 
-            <hr className='hr' />
+                <hr className='hr' />
 
-            <p>
-                Hello and welcome to my online gallery! I'm glad you made it :-] this is where I'll be posting updates on my latest projects and selling selected works. If you have any questions, please feel free to follow the info link you see in the nav bar. Thanks for stopping by!
-            </p>
+                <p>
+                    Hello and welcome to my online gallery! I'm glad you made it :-] this is where I'll be posting updates on my latest projects and selling selected works. If you have any questions, please feel free to follow the info link you see in the nav bar. Thanks for stopping by!
+                </p>
 
-            <p>- Tom</p>
+                <p>- Tom</p>
+            </div>
         </header>
     )
 }
